4912b0b0e6ffc4d8393cc226835cb9ec04535b95
VladimirYazynin <vladimir.yazynin@inbox.ru>
2025-06-21 01:43:18 +0300

    Исправить FilmController добавив взаимодействие с данными через сервисный слой

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -0,0 +1,38 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+
+import java.util.Collection;
+
+@Service
+@RequiredArgsConstructor
+public class FilmService {
+
+    private final FilmStorage filmStorage;
+
+    public Film addFilm(Film film) {
+        return filmStorage.addFilm(film);
+    }
+
+    public Film updateFilm(Film modifiedFilm) {
+        return filmStorage.updateFilm(modifiedFilm)
+                        .orElseThrow(() -> new NotFoundException(
+                                "Фильм с id: " + modifiedFilm.getId() +  " не найден."
+                        ));
+    }
+
+    public Collection<Film> getAllFilms() {
+        return filmStorage.getAllFilms();
+    }
+
+
+    // добавить лайк
+
+    // удалить лайк
+
+    // вывести топ 10
+}
