5541a42c81e3bc322d88de00570b9a4264fd3b55
Vladimir <vladimir.yazynin@inbox.ru>
2025-07-01 19:41:22 +0300

    Добавить методы сохранения и удаления друзей в БД

--- src/main/java/ru/yandex/practicum/filmorate/dal/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/storage/user/UserDbStorage.java
@@ -17,6 +17,18 @@ public class UserDbStorage extends DbStorage implements UserStorage {
     private final static String INSERT_QUERY = "INSERT INTO users(email, login, name, birthday) VALUES (?, ?, ?, ?)";
     private final static String UPDATE_QUERY = "UPDATE users SET email = ?, login = ?, name = ?, birthday = ? WHERE id = ?";
     private final static String DELETE_QUERY = "DELETE FROM users WHERE id = ?";
+    private final static String ADD_FRIEND_QUERY = "INSERT INTO friends(user_id, friend_id) VALUES (?, ?)";
+    private final static String DELETE_FRIEND_QUERY = "DELETE FROM friends WHERE user_id = ? AND friend_id = ?";
+    private final static String FIND_USER_FRIENDS_QUERY = "SELECT u.id, u.email, u.login, u.name, u.birthday FROM friends AS f INNER JOIN users AS u ON f.friend_id = u.id WHERE f.user_id = ?";
+    private final static String FIND_USERS_COMMON_FRIENDS_QUERY = "SELECT u.id, u.email, u.login, u.name, u.birthday " +
+                                                                    " FROM friends f" +
+                                                                    " JOIN users u ON f.friend_id = u.id" +
+                                                                    " WHERE f.user_id = ?" +
+                                                                    " INTERSECT" +
+                                                                    " SELECT u.id, u.name, u.email, u.login, u.birthday" +
+                                                                    " FROM friends f" +
+                                                                    " JOIN users u ON f.friend_id = u.id" +
+                                                                    " WHERE f.user_id = ?";
 
 
     public UserDbStorage(JdbcTemplate jdbc, RowMapper<User> mapper) {
@@ -65,21 +77,33 @@ public class UserDbStorage extends DbStorage implements UserStorage {
 
     @Override
     public void addFriend(long userId, long friendId) {
-
+        insert(
+                ADD_FRIEND_QUERY,
+                userId,
+                friendId
+        );
     }
 
     @Override
     public void deleteFriend(long userId, long friendId) {
-
+        update(
+                DELETE_FRIEND_QUERY,
+                userId,
+                friendId
+        );
     }
 
     @Override
     public Collection<User> getUserFriends(long userId) {
-        return null;
+        return findMany(FIND_USER_FRIENDS_QUERY, userId);
     }
 
     @Override
     public Collection<User> getCommonFriends(long userId, long otherId) {
-        return null;
+        return findMany(
+                FIND_USERS_COMMON_FRIENDS_QUERY,
+                userId,
+                otherId
+        );
     }
 }
